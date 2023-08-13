b374bab78823674e3bb700a30ae49dadefc38b0d
Glori <75176699+globv28@users.noreply.github.com>
2023-08-13 09:26:17 -0600

    Gameover screeen
    Tiene 3 estados de fin del juego

--- Memories/Assets/Scripts/Timer.cs
+++ Memories/Assets/Scripts/Timer.cs
@@ -7,6 +7,8 @@ public class Timer : MonoBehaviour
 {
     public float timer = 0;
     public TMP_Text textoTimer;
+    public Inventario inventario;
+    public GameManager manager;
 
     // Update is called once per frame
     void Update()
@@ -14,9 +16,22 @@ public class Timer : MonoBehaviour
         timer -= Time.deltaTime;
         textoTimer.text = "" + timer.ToString("f2");
         
-        if(timer < 0)
+        if(timer < 0 && inventario.cantObj < manager.objLimit)
         {
+
             textoTimer.text = "Game Over";
+            if(inventario.cantNorm > inventario.cantTrans)
+            {
+                manager.gameOver("La verdad soy toda, y la queso");
+            } 
+            else if(inventario.cantTrans >= inventario.cantNorm)
+            {
+                manager.gameOver("Soy lo menos, que triste la vida");
+    
+            } 
+        } else if(inventario.cantObj == manager.objLimit)
+        {
+            manager.gameOver("La vida da muchas vueltas, a ver que sigue");
         }
     }
 }
